d5cebce0b3b49117983a60c8824b0057d4913b95
Nicolas <nicolasmarquez005@gmail.com>
2023-03-01 21:44:11 -0300

    top score

--- back-end/controllers/stats.js
+++ back-end/controllers/stats.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router();
 const { UserStats } = require('../models/userStats')
-const { User } = require('../models/user')
+const { User } = require('../models/user');
+const sequelize = require('../infraestructure/db');
 
 router.get('/', async (req, res) => {
     const userId = req.get("userId")
@@ -11,6 +12,13 @@ router.get('/', async (req, res) => {
         }
     })
     res.status(201).send(JSON.stringify(stats))
+})
+
+router.get('/all', async (req, res) => {
+    const stats = await sequelize.query(
+        "SELECT name, points FROM user_stats JOIN users ON user_stats.user_id = users.id"
+    )
+    res.status(200).send(JSON.stringify(stats[0]))
 })
 
 module.exports = router
