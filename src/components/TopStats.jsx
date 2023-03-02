386b0b0533bdb65d1757992ba732d2bd71f830b2
Nicolas <nicolasmarquez005@gmail.com>
2023-03-01 21:44:40 -0300

    top score

--- src/components/TopStats.jsx
+++ src/components/TopStats.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const TopStats = () => {
+    const [list, setList] = useState([])
+
+    const url = "http://localhost:8080/stats/all"
+
+    useEffect(() => {
+        getAllStats()
+
+        async function getAllStats(){
+            await fetch(url,{
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                }
+            })
+                .then((response) => response.json())
+                .then((data) =>{
+                    setList(data)})
+        }
+    }, [])
+
+
     return(
         <div className="topStats">
-            <p>andljkandfñljksdnfñjklsdnfñlk</p>
+            <h1>TOP PUNTOS</h1>
+            <div>
+                {
+                    list.map((user, key) => {
+                        return(
+                            <p key={key}>{user.name}: {user.points}</p>
+                        )                 
+                    })
+                }
+            </div>
         </div>
     )
 }
