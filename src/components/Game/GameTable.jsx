03f1391f3954dd58cee90a7992ecb1ecb24cff49
Nicolas <nicolasmarquez005@gmail.com>
2023-03-11 18:49:16 -0300

    colores en letras

--- src/components/Game/GameTable.jsx
+++ src/components/Game/GameTable.jsx
@@ -63,7 +63,7 @@ const GameTable = (props) => {
         if(table[activeRow].toString().replace(/,/g,"").toLowerCase() === props.word){
             console.log("ESA")
             setIsWinner(true)
-            handleGameOver(isWinner)
+            handleGameOver(true)
         }
         if(activePosition < 5) return
         if(activeRow == 5){
@@ -77,46 +77,46 @@ const GameTable = (props) => {
     return(
         <div className="table" onKeyDown={(e) => handleKeyDown(e)}>
             <div className="inputRow">
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={0} position={0}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={0} position={1}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={0} position={2}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={0} position={3}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={0} position={4}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={0} position={0}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={0} position={1}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={0} position={2}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={0} position={3}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={0} position={4}/>
             </div>
             <div className="inputRow">
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={1} position={0}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={1} position={1}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={1} position={2}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={1} position={3}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={1} position={4}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={1} position={0}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={1} position={1}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={1} position={2}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={1} position={3}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={1} position={4}/>
             </div>
             <div className="inputRow">
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={2} position={0}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={2} position={1}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={2} position={2}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={2} position={3}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={2} position={4}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={2} position={0}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={2} position={1}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={2} position={2}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={2} position={3}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={2} position={4}/>
             </div>
             <div className="inputRow">
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={3} position={0}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={3} position={1}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={3} position={2}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={3} position={3}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={3} position={4}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={3} position={0}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={3} position={1}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={3} position={2}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={3} position={3}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={3} position={4}/>
             </div>
             <div className="inputRow">
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={4} position={0}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={4} position={1}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={4} position={2}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={4} position={3}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={4} position={4}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={4} position={0}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={4} position={1}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={4} position={2}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={4} position={3}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={4} position={4}/>
             </div>
             <div className="inputRow">
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={5} position={0}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={5} position={1}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={5} position={2}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={5} position={3}/>
-                <Letter table={table} activeRow={activeRow} activePosition={activePosition} attempt={5} position={4}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={5} position={0}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={5} position={1}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={5} position={2}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={5} position={3}/>
+                <Letter table={table} word={props.word} activeRow={activeRow} activePosition={activePosition} attempt={5} position={4}/>
             </div>
 
             <button type="button" onClick={handleSend}>ENVIAR</button>
--- src/components/Game/Letter.jsx
+++ src/components/Game/Letter.jsx
@@ -1,16 +1,33 @@
 import React, { useEffect, useState } from "react";
 
 const Letter = (props) => {
-
-    const [active, setActive] = useState(false)
     const board = props.table
+    const letter = board[props.attempt][props.position]
+    const [active, setActive] = useState(false)
+    const [correct, setCorrect] = useState(false)
+    const [close, setClose] = useState(false)
+    const [incorrect, setIncorrect] = useState(false)
+    /*const correct = board[props.attempt][props.position] == props.word[props.position]
+    const close = !correct && board[props.attempt][props.position] != "" && props.word.includes(board[props.attempt][props.position])
+    const letterState = props.activeRow > props.attempt ? (correct ? "correct" : close ? "close" : "incorrect") : ""*/
     useEffect(()=>{
-        console.log(props.activeRow, props.activePosition)
+        if(letter.toLowerCase() == props.word[props.position]){
+            console.log("TRUE LETRA BIEN")
+            setCorrect(true)
+        } else if (letter != "" && props.word.includes(letter.toLowerCase())){
+            setClose(true)
+        } else if(letter != ""){
+            setIncorrect(true)
+        }
+    },[props.activeRow])
+
+    useEffect(()=>{
         if(props.activeRow == props.attempt && props.activePosition == props.position){
             setActive(true)
         } else {
             setActive(false)
         }
+
     },[props.table])
 
     /*useEffect(()=>{
@@ -18,7 +35,7 @@ const Letter = (props) => {
     },[])*/
 
     return(
-        <div className={`${active ? 'active':'letter'}`}>{board[props.attempt][props.position]}</div>
+        <div id={`${correct?"correct":close?"close":incorrect?"incorrect": ""}`} className={`${active ? 'active':'letter'}`}>{letter}</div>
     )
 }
 
